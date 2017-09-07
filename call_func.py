28e8ec350d1085ff7ec1593a1719703be9d998f7
Tower-Cassie <2717062155@qq.com>
2017-09-07 15:41:26 +0800

    Call function from Python

---
+++ call_func.py
@@ -0,0 +1,14 @@
+#!/Users/xiaohong/Desktop/Python3.5
+#Filename:call_func.py
+#_*_ coding:utf-8 _*_
+
+
+x = abs(100)
+print(x)
+
+y = abs(-20)
+print(y)
+
+print("The max value is:",max(1,45,78))
+print("The min value is:",min(23,45,78))
+print("The sum of the numbers is :",sum([12,52,45]))
